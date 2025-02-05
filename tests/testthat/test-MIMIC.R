0f3bc5b4f448394783a983607e200550a3a7d894
Jannik Orzek <45204741+jhorzek@users.noreply.github.com>
2025-02-05 23:29:57 +0100

    test-MIMIC.R aktualisieren

--- tests/testthat/test-MIMIC.R
+++ tests/testthat/test-MIMIC.R
@@ -2,13 +2,18 @@ test_that("MIMIC works", {
   library(mxsem)
   library(lavaan)
   library(bnSEM)
+  set.seed(73)
+  sim_model <- "
+  eta =~ 1*y1 + 1*y2 + 1*y3 + 1*y4
+  eta ~ .3*x1 + .5*x2 + .2*x3 + .1*x4 + .1*x5
+  "
+
+  suppressWarnings(data <- lavaan::simulateData(sim_model))
+
   model <- "
   eta =~ y1 + y2 + y3 + y4
   eta ~  x1 + x2 + x3 + x4 + x5
   "
-
-  suppressWarnings(data <- lavaan::simulateData(model))
-
   fit_mx <- mxsem(model,
                   data = data) |>
     mxTryHard()
